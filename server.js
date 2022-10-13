0cbc69c77922ed69b65d4283babbf3146af8e718
AWS <noreply-aws@amazon.com>
2022-10-13 12:09:57 +0000

    Initial commit made by AWS during repository creation.

---
+++ server.js
@@ -0,0 +1,19 @@
+var port = process.env.PORT || 3000;
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(req, res) {
+  res.send({
+    "Output": "Hello World!"
+  });
+});
+
+app.post('/', function(req, res) {
+  res.send({
+    "Output": "Hello World!"
+  });
+});
+
+app.listen(port);
+module.exports = app;
